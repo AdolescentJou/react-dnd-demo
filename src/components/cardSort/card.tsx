b097a0a90c446e2f4abaafca0111d06fdf24d86f
jasperou <jasperou@tencent.com>
2022-10-11 10:41:23 +0800

    feat:卡片排序hover

--- src/components/cardSort/card.tsx
+++ src/components/cardSort/card.tsx
@@ -3,7 +3,6 @@ import { useDrag, useDrop } from 'react-dnd';
 
 const Card = ({ text, id, index, changePosition }: any) => {
   const ref = useRef(null);
-  const [hoverId, setHoverId] = useState(id);
   // 因为没有定义收集函数，所以返回值数组第一项不要
   const [, drop] = useDrop({
     accept: 'DragDropBox',
@@ -11,11 +10,8 @@ const Card = ({ text, id, index, changePosition }: any) => {
       if (!ref.current) return;
       let dragIndex = item.index;
       let hoverIndex = index;
-      console.log('dragIndex', dragIndex, 'hoverIndex', hoverIndex);
-
       if (dragIndex === hoverIndex) return; // 如果回到自己的坑，那就什么都不做
       changePosition(dragIndex, hoverIndex); // 调用传入的方法完成交换
-      setHoverId(() => item.index);
       item.index = hoverIndex; // 将当前当前移动到Box的index赋值给当前拖动的box，不然会出现两个盒子疯狂抖动！
     },
     drop: (item, monitor) => {},
@@ -25,17 +21,20 @@ const Card = ({ text, id, index, changePosition }: any) => {
     type: 'DragDropBox',
     item: { id, index },
     end: () => {},
+    isDragging: (monitor) => {
+      return index === monitor.getItem().index;
+    },
     collect: (monitor) => ({
-      isDragging: monitor.isDragging(), // css样式需要
+      isDragging: monitor.isDragging(),
     }),
   });
 
   return (
     <div
       className='card_drag'
-      ref={drag(drop(ref)) as any}
+      ref={drag(drop(ref)) as any} // 这样写可以让它即接收拖拽又实现拖拽
       style={{
-        opacity: isDragging && id === hoverId ? 0.3 : 1,
+        opacity: isDragging ? 0.3 : 1,
       }}
     >
       {text}
